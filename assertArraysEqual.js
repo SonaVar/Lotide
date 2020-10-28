f22a9cf67a8b587d2c89feef98bac1c3c500d981
Sona Varghese <sonasusan.v@gmail.com>
2020-10-28 00:49:42 +0000

    Commiy assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = function(array1, array2) {
+  for (let i = 0; i < array1.length; i ++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  console.log ((eqArrays(arrayOne, arrayTwo)) ? `✅✅✅ ${arrayOne} === ${arrayTwo}` : `🛑🛑🛑 ${arrayOne} !== ${arrayTwo}`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
