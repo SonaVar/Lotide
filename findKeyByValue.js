5c554699d6ad4b92d17aecf9455734596651485c
Sona Varghese <sonasusan.v@gmail.com>
2020-10-29 01:00:29 +0000

    Commit findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
+
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  for (let element in object) {
+    if (object[element] === value) {
+      return element;
+    }
+  }
+  return undefined;
+};
+
+// TEST CODE
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Friends"), undefined);
