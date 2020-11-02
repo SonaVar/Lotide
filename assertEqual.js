0c2c0f6af60c3423004db307396c59e5b61b5d85
Sona Varghese <sonasusan.v@gmail.com>
2020-11-02 16:40:44 +0000

    Commit changes of WET to DRY

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ ${actual} === ${expected}`);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Dona", "Dona");
+module.exports = assertEqual;
