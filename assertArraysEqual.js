342c1c02a018763c3799abe1cb6d00f3c8bf49e2
Sona Varghese <sonasusan.v@gmail.com>
2020-11-02 17:07:39 +0000

    Commit WET to DRY assertArraysEqual.js and assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,7 @@
-const eqArrays = function(array1, array2) {
-  for (let i = 0; i < array1.length; i ++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('../lotide/eqArrays');
 
 const assertArraysEqual = function(arrayOne, arrayTwo) {
-  console.log((eqArrays(arrayOne, arrayTwo)) ? `✅✅✅ ${arrayOne} === ${arrayTwo}` : `🛑🛑🛑 ${arrayOne} !== ${arrayTwo}`);
+  console.log((eqArrays(arrayOne, arrayTwo)) ? `✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}` : `🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+module.exports = assertArraysEqual;
